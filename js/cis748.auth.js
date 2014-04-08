ee39ed2712f68f8384052501303e6281ce30e20a
sneddon <jack2760@gmail.com>
2014-04-07 20:07:19 -0600

    Initial Checkin
    Based on OAuth 2.0 Example

---
+++ js/cis748.auth.js
@@ -0,0 +1,65 @@
+//set scope
+var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
+
+// This function is called after the Client Library has finished loading
+function handleClientLoad() {
+
+  // 1. Set the API Key
+  gapi.client.setApiKey(apiKey);
+
+  // 2. Call the function that checks if the user is Authenticated. This is defined in the next section
+  window.setTimeout(checkAuth,1);
+}
+
+function checkAuth() {
+  // Call the Google Accounts Service to determine the current user's auth status.
+  // Pass the response to the handleAuthResult callback function
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+  if (authResult) {
+    // The user has authorized access
+
+    console.log(authResult.status);
+
+    handleAuthorized();
+
+  } else {
+    // User has not Authenticated and Authorized
+    handleUnAuthorized();
+  }
+}
+
+// Authorized user
+function handleAuthorized() {
+    console.log("handleAuthorized");
+
+    // display happy chuck
+    $('#chuckHappy').show();
+    $('#chuckNotHappy').hide();
+
+    // display logout button
+    $('#logout-button').show();
+    $('#authorize-button').hide();
+}
+
+// Unauthorized user
+function handleUnAuthorized() {
+    console.log("handleUnAuthorized");
+
+    // display happy chuck
+    $('#chuckHappy').hide();
+    $('#chuckNotHappy').show();
+
+    // display logout button
+    $('#logout-button').hide();
+    $('#authorize-button').show();
+
+}
+
+
+$("#authorize-button").on("click", function (e){
+  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+  return false;
+});
---
+++ js/cis748.keys.js
@@ -0,0 +1,3 @@
+// google api keys
+var clientId = '165327219674-mrd6uk3mj7r9uetlfejeqitd504qg8o9.apps.googleusercontent.com';
+var apiKey = 'AIzaSyAc7YrXjqhjGuKmO7IqdziAUmAdt43bnhk';
