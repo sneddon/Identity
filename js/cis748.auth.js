797e47878f74e0264f4ad753e9caeb51e61d3cf4
sneddon <jack2760@gmail.com>
2014-04-07 21:39:43 -0600

    start using google+ apis

--- js/cis748.auth.js
+++ js/cis748.auth.js
@@ -4,17 +4,31 @@ var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
 // This function is called after the Client Library has finished loading
 function handleClientLoad() {
 
+  console.log("handleClientLoad - Begin");
+  console.log("options = " + options );
   // 1. Set the API Key
-  gapi.client.setApiKey(apiKey);
+  // gapi.client.setApiKey(apiKey);
+  gapi.auth.signIn(options);
 
   // 2. Call the function that checks if the user is Authenticated. This is defined in the next section
   window.setTimeout(checkAuth,1);
+
+  console.log("handleClientLoad - End");
 }
 
-function checkAuth() {
+var checkAuth = function(authResult) {
   // Call the Google Accounts Service to determine the current user's auth status.
   // Pass the response to the handleAuthResult callback function
-  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+  //gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+
+  console.log("checkAuth - Begin");
+
+  if (authResult) {
+    console.log(authResult);
+  }
+
+  console.log("checkAuth - End");
+
 }
 
 function handleAuthResult(authResult) {
--- js/cis748.keys.js
+++ js/cis748.keys.js
@@ -1,3 +1,11 @@
 // google api keys
-var clientId = '165327219674-mrd6uk3mj7r9uetlfejeqitd504qg8o9.apps.googleusercontent.com';
-var apiKey = 'AIzaSyAc7YrXjqhjGuKmO7IqdziAUmAdt43bnhk';
+//var clientId = '165327219674-mrd6uk3mj7r9uetlfejeqitd504qg8o9.apps.googleusercontent.com';
+//var apiKey = 'AIzaSyAc7YrXjqhjGuKmO7IqdziAUmAdt43bnhk';
+
+var options = {
+  'callback' : checkAuth,
+  'approvalprompt' : 'force',
+  'clientid' : '165327219674-mrd6uk3mj7r9uetlfejeqitd504qg8o9.apps.googleusercontent.com',
+  'requestvisibleactions' : 'http://schemas.google.com/CommentActivity http://schemas.google.com/ReviewActivity',
+  'cookiepolicy' : 'single_host_origin'
+};
