2afedc52bcf5ec2eee3342b2963daac98bbe90ef
sneddon <jack2760@gmail.com>
2014-04-07 21:46:02 -0600

    added debug string

--- js/identify.auth.js
+++ js/identify.auth.js
@@ -32,6 +32,7 @@ var checkAuth = function(authResult) {
 }
 
 function handleAuthResult(authResult) {
+  console.log("handleAuthResult - Begin");
   if (authResult) {
     // The user has authorized access
 
