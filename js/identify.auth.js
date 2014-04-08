10815d8a62a437b017aaba57eb447607087e2eb8
sneddon <jack2760@gmail.com>
2014-04-07 22:18:40 -0600

    added authResult logging

--- js/identify.auth.js
+++ js/identify.auth.js
@@ -13,27 +13,21 @@ var options = {
 
 // This function is called after the Client Library has finished loading
 function handleClientLoad() {
-
-  console.log("handleClientLoad - Begin");
-  // 1. Set the API Key
-  // gapi.client.setApiKey(apiKey);
+  // 1. SignIn with the options
   gapi.auth.signIn(options);
 
   // 2. Call the function that checks if the user is Authenticated. This is defined in the next section
   window.setTimeout(checkAuth,1);
-
-  console.log("handleClientLoad - End");
 }
 
 var checkAuth = function(authResult) {
   // Call the Google Accounts Service to determine the current user's auth status.
   // Pass the response to the handleAuthResult callback function
-  //gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
-
-  console.log("checkAuth - Begin");
-
   if (authResult) {
-    console.log(authResult);
+    console.log(authResult['status']['google_logged_in']);
+  }
+  else {
+    console.log("no authResult!");
   }
 
   console.log("checkAuth - End");
